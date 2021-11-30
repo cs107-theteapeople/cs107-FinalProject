3980e5b0db8298b5b326956b120ac2344449986e
David Berthiaume <david@papercrane.io>
2021-11-30 13:56:36 -0500

    Updated tests to work with vector inputs

--- tests/AD_test.py
+++ tests/AD_test.py
@@ -25,93 +25,93 @@ def test_mul():
     # test multiplication with a constant
     x = ad.var('x')
     f = x * 3
-    assert f.eval(x=3) == {'value': 9, 'derivative': 3}, "Error: incorrect multiplication"
+    assert f.eval(x=3) == {'value': 9, 'derivative': {'x':3}}, "Error: incorrect multiplication"
 
 
 def test_rmul():
     # test (right) multiplication with a constant
     x = ad.var('x')
     f = 3 * x
-    assert f.eval(x=3) == {'value': 9, 'derivative': 3}, "Error: incorrect right multiplication"
+    assert f.eval(x=3) == {'value': 9, 'derivative': {'x':3}}, "Error: incorrect right multiplication"
 
 
 def test_add():
     # test simple addition
     x = ad.var('x')
     f = x + 3
-    assert f.eval(x=3) == {'value': 6, 'derivative': 1}, "Error: incorrect addition"
+    assert f.eval(x=3) == {'value': 6, 'derivative': {'x':1}}, "Error: incorrect addition"
 
 
 def test_radd():
     # test right addition
     x = ad.var('x')
     f = 3 + x
-    assert f.eval(x=3) == {'value': 6, 'derivative': 1}, "Error: incorrect right addition"
+    assert f.eval(x=3) == {'value': 6, 'derivative': {'x':1}}, "Error: incorrect right addition"
 
 
 def test_pow():
     # test raising to a constant power
     x = ad.var('x')
     f = x ** 2
-    assert f.eval(x=3) == {'value': 9, 'derivative': 6}, "Error: incorrect power"
+    assert f.eval(x=3) == {'value': 9, 'derivative': {'x':6}}, "Error: incorrect power"
 
 
 def test_rpow():
     # test raising a constant to a variable power
     x = ad.var('x')
     f = math.e ** x
-    assert f.eval(x=2) == {'value': math.e ** 2, 'derivative': math.e ** 2}, "Error: incorrect right power"
+    assert f.eval(x=2) == {'value': math.e ** 2, 'derivative': {'x':math.e ** 2}}, "Error: incorrect right power"
 
 
 def test_neg():
     # test negation and adding of negative constants
     x = ad.var('x')
     f = -x
-    assert f.eval(x=3) == {'value': -3, 'derivative': -1}, "Error: incorrect negative operation"
+    assert f.eval(x=3) == {'value': -3, 'derivative': {'x':-1}}, "Error: incorrect negative operation"
     g = - 5 + x
-    assert g.eval(x=1) == {'value': -4, 'derivative': 1}, "Error: incorrect negative operation"
+    assert g.eval(x=1) == {'value': -4, 'derivative': {'x':1}}, "Error: incorrect negative operation"
 
 
 def test_truediv():
     # test dividing by a constant
     x = ad.var('x')
     f = x / 3
-    assert f.eval(x=3) == {'value': 1, 'derivative': 1 / 3}, "Error: incorrect truediv"
+    assert f.eval(x=3) == {'value': 1, 'derivative': {'x':1 / 3}}, "Error: incorrect truediv"
 
 
 def test_rtruediv():
     # test dividing a constant by a variable
     x = ad.var('x')
     f = 3 / x
-    assert f.eval(x=3) == {'value': 1, 'derivative': - 1 / 3}, "Error: incorrect right truediv"
+    assert f.eval(x=3) == {'value': 1, 'derivative': {'x':- 1 / 3}}, "Error: incorrect right truediv"
 
 
 def test_sub():
     # test subtraction
     x = ad.var('x')
     f = x - 3
-    assert f.eval(x=1) == {'value': -2, 'derivative': 1}, "Error: incorrect subtraction"
+    assert f.eval(x=1) == {'value': -2, 'derivative': {'x':1}}, "Error: incorrect subtraction"
 
 
 def test_rsub():
     # test right subtraction
     x = ad.var('x')
     f = 3 - x
-    assert f.eval(x=2) == {'value': 1, 'derivative': -1}, "Error: incorrect right subtraction"
+    assert f.eval(x=2) == {'value': 1, 'derivative': {'x':-1}}, "Error: incorrect right subtraction"
 
 
 def test_sin():
     # test the sin function
     x = ad.var('x')
     f = ad.sin(x)
-    assert f.eval(x=1) == {'value': np.sin(1), 'derivative': np.cos(1)}, "Error: incorrect sine function"
+    assert f.eval(x=1) == {'value': np.sin(1), 'derivative': {'x':np.cos(1)}}, "Error: incorrect sine function"
 
 
 def test_cos():
     # test the cosine function
     x = ad.var('x')
     f = ad.cos(x)
-    assert f.eval(x=1) == {'value': np.cos(1), 'derivative': -np.sin(1)}, "Error: incorrect cosine function"
+    assert f.eval(x=1) == {'value': np.cos(1), 'derivative': {'x':-np.sin(1)}}, "Error: incorrect cosine function"
 
 
 def test_tan():
@@ -119,14 +119,15 @@ def test_tan():
     x = ad.var('x')
     f = ad.tan(x)
     assert f.eval(x=1) == {'value': np.tan(1),
-                           'derivative': 1 / ((np.cos(1)) ** 2)}, "Error: incorrect tangent function"
+                           'derivative': {'x':1 / ((np.cos(1)) ** 2)}}, "Error: incorrect tangent function"
 
 
 def test_eval_incorrect_var():
     # test to see if we enter incorrect variable names in eval
     x = ad.var('x')
     f = x + 3
-    assert f.eval(y=3) == None, "Error: does not deal with incorrect variables in evaluation as expected"
+    with pytest.raises(ValueError):
+        f.eval(y=3)
 
 def test_to_complex():
     # make sure we raise an exception when complex values would be created
@@ -142,28 +143,28 @@ def test_func_comp():
     # here we test a set of compound function to ensure that the chain rule is working correctly
     x = ad.var('x')
     f1 = ad.sin(2 * x)
-    assert f1.eval(x=1) == {'value': np.sin(2), 'derivative': 2 * np.cos(2)}, "Error: function composite – sin(2x)"
+    assert f1.eval(x=1) == {'value': np.sin(2), 'derivative': {'x':2 * np.cos(2)}}, "Error: function composite – sin(2x)"
 
     f2 = ad.exp(2 * ad.sin(x ** 2))
-    assert f2.eval(x=1) == {'value': np.exp(2 * np.sin(1)), 'derivative': 4 * np.exp(2 * np.sin(1)) * np.cos(
-        1)}, "Error: function composite – exp(2*sin(x**2))"
+    assert f2.eval(x=1) == {'value': np.exp(2 * np.sin(1)), 'derivative': {'x':4 * np.exp(2 * np.sin(1)) * np.cos(
+        1)}}, "Error: function composite – exp(2*sin(x**2))"
 
     f3 = x ** (ad.tan(x))
-    val3, der3 = f3.eval(x=2)['value'], f3.eval(x=2)['derivative']
+    val3, der3 = f3.eval(x=2)['value'], f3.eval(x=2)['derivative']['x']
     assert np.isclose(val3, 2 ** np.tan(2)) and np.isclose(der3, 2 ** np.tan(2) * (
                 np.tan(2) / 2 + np.log(2) / (np.cos(2) ** 2))), "Error: function composite – exp(2*sin(x**2))"
 
     f4 = 2 ** ad.tan(ad.exp(x))
-    val4, der4 = f4.eval(x=3)['value'], f4.eval(x=3)['derivative']
+    val4, der4 = f4.eval(x=3)['value'], f4.eval(x=3)['derivative']['x']
     assert np.isclose(val4, 7.332336) and np.isclose(der4, 945.4314), "Error: function composite - 2**(tan(exp(x)))"
 
     f5 = ad.sin(x) * ad.cos(x ** 2) * ad.exp(7 * x)
-    val5, der5 = f5.eval(x=1.1111)['value'], f5.eval(x=1.1111)['derivative']
+    val5, der5 = f5.eval(x=1.1111)['value'], f5.eval(x=1.1111)['derivative']['x']
     assert np.isclose(val5, 705.7684) and np.isclose(der5,
                                                      802.6919), "Error: function composite - sin(x)cos(x**2)exp(7x)"
 
     f6 = ad.exp(3 * ad.tan(2 ** x))
-    val6, der6 = f6.eval(x=1)['value'], f6.eval(x=1)['derivative']
+    val6, der6 = f6.eval(x=1)['value'], f6.eval(x=1)['derivative']['x']
     assert np.isclose(val6, np.exp(3 * np.tan(2))) and np.isclose(der6,
            0.034169), "Error: function composite - sin(x)cos(x**2)exp(7x)"
 
@@ -197,7 +198,7 @@ def newton_solver(func, x, x0, precision, max_iter):
 
     iteration = 0
     while abs(func.eval(x=x0)['value']) > precision and iteration < max_iter:
-        x0 = x0 - func.eval(x=x0)['value'] / func.eval(x=x0)['derivative']
+        x0 = x0 - func.eval(x=x0)['value'] / func.eval(x=x0)['derivative']['x']
         iteration += 1
     if iteration == max_iter:
         print(f"Cannot find root with Newton's method with {max_iter} iterations.")
@@ -225,23 +226,23 @@ def test_constants():
 
     f = y + b
     results = f.eval(y=1)
-    assert results['value'] == 5 and results['derivative'] == 1
+    assert results['value'] == 5 and results['derivative']['y'] == 1
 
     f = b + y
     results = f.eval(y=1)
-    assert results['value'] == 5 and results['derivative'] == 1
+    assert results['value'] == 5 and results['derivative']['y'] == 1
 
     f = y * b
     results = f.eval(y=1)
-    assert results['value'] == 4 and results['derivative'] == 4
+    assert results['value'] == 4 and results['derivative']['y'] == 4
 
     f = b ** y
     results = f.eval(y=2)
-    assert results['value'] == 16 and np.isclose(results['derivative'], np.log(4) * 4 ** 2)
+    assert results['value'] == 16 and np.isclose(results['derivative']['y'], np.log(4) * 4 ** 2)
 
     f = y ** b
     results = f.eval(y=2)
-    assert results['value'] == 16 and np.isclose(results['derivative'], 4 * (2 ** 3))
+    assert results['value'] == 16 and np.isclose(results['derivative']['y'], 4 * (2 ** 3))
 
 def test_literals():
     # here we test to make sure that ordinary integers and floats work OK with operator overloading
@@ -250,24 +251,24 @@ def test_literals():
 
     f = y + b
     results = f.eval(y=1)
-    assert results['value'] == 5 and results['derivative'] == 1
+    assert results['value'] == 5 and results['derivative']['y'] == 1
 
     f = b + y
     results = f.eval(y=1)
-    assert results['value'] == 5 and results['derivative'] == 1
+    assert results['value'] == 5 and results['derivative']['y'] == 1
 
     f = y * b
     results = f.eval(y=1)
-    assert results['value'] == 4 and results['derivative'] == 4
+    assert results['value'] == 4 and results['derivative']['y'] == 4
 
     f = b ** y
     results = f.eval(y=2)
     print (results)
-    assert results['value'] == 16 and np.isclose(results['derivative'], np.log(4) * 4 ** 2)
+    assert results['value'] == 16 and np.isclose(results['derivative']['y'], np.log(4) * 4 ** 2)
 
     f = y ** b
     results = f.eval(y=2)
-    assert results['value'] == 16 and np.isclose(results['derivative'], 4 * (2 ** 3))
+    assert results['value'] == 16 and np.isclose(results['derivative']['y'], 4 * (2 ** 3))
 
 def test_divide():
     # here we test a few functions that have a division in them
@@ -275,11 +276,11 @@ def test_divide():
 
     f = x / 4
     results = f.eval(x=4)
-    assert results['value'] == 1 and np.isclose(results['derivative'] , 1/4)
+    assert results['value'] == 1 and np.isclose(results['derivative']['x'] , 1/4)
 
     f = 4 / x
     results = f.eval(x=4)
-    assert results['value'] == 1 and np.isclose(results['derivative'] , -4 / (4**2))
+    assert results['value'] == 1 and np.isclose(results['derivative']['x'] , -4 / (4**2))
 
 def test_subtraction():
     # here we test a few functions that have a division in them
@@ -287,19 +288,19 @@ def test_subtraction():
 
     f = x - ad.const(4)
     results = f.eval(x=4)
-    assert results['value'] == 0 and results['derivative'] == 1
+    assert results['value'] == 0 and results['derivative']['x'] == 1
 
     f = ad.const(4) - x
     results = f.eval(x=4)
-    assert results['value'] == 0 and results['derivative'] == -1
+    assert results['value'] == 0 and results['derivative']['x'] == -1
 
     f = 4 - x
     results = f.eval(x=4)
-    assert results['value'] == 0 and results['derivative'] == -1
+    assert results['value'] == 0 and results['derivative']['x'] == -1
 
     f = x - 4
     results = f.eval(x=4)
-    assert results['value'] == 0 and results['derivative'] == 1
+    assert results['value'] == 0 and results['derivative']['x'] == 1
 
 
 
@@ -319,7 +320,7 @@ def test_subtract_self():
     x = ad.var('x')
     f = x - x
     results = f.eval(x=4)
-    assert results['value'] == 0 and results['derivative'] == 0
+    assert results['value'] == 0 and results['derivative']['x'] == 0
 
 def test_add_function():
     # test the add function
@@ -328,15 +329,15 @@ def test_add_function():
     x = ad.var('x')
     f = ad.add(x, 2)
     results = f.eval(x=4)
-    assert results['value'] == 6 and results['derivative'] == 1
+    assert results['value'] == 6 and results['derivative']['x'] == 1
 
     f = ad.add(x, x)
     results = f.eval(x=4)
-    assert results['value'] == 8 and results['derivative'] == 2
+    assert results['value'] == 8 and results['derivative']['x'] == 2
 
     f = ad.add(2, x)
     results = f.eval(x=4)
-    assert results['value'] == 6 and results['derivative'] == 1
+    assert results['value'] == 6 and results['derivative']['x'] == 1
 
 def test_bad_constant():
     # this should raise an exception
@@ -349,7 +350,7 @@ def test_self_divide():
     x = ad.var('x')
     f = x / x
     results = f.eval(x=3)
-    assert results['value'] == 1 and results['derivative'] == 0
+    assert results['value'] == 1 and results['derivative']['x'] == 0
 
 def test_enter_bad_values():
     # test entering a string into the eval function
@@ -360,3 +361,4 @@ def test_enter_bad_values():
     # entering a string into eval should raise a value error
     with pytest.raises(ValueError):
         f.eval(x='Chicken!')
+
